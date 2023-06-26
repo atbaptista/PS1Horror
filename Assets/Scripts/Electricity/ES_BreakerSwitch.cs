a3b4708dd9da5625cebdf61928241d7bccfa26c6
Alex Baptista <01abaptista@gmail.com>
2023-06-26 19:03:16 -0400

    documentation

--- Assets/Scripts/Electricity/ES_BreakerSwitch.cs
+++ Assets/Scripts/Electricity/ES_BreakerSwitch.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//the gameobject with the breakerbox component needs to be the parent of all the switches
 public class ES_BreakerSwitch : MonoBehaviour, Interactable
 {
-    public ES_BreakerBox breakerBox;
+    [HideInInspector] public ES_BreakerBox breakerBox;
     public GameObject poweredObject;
+
+    private Powerable _powerableComponent;
 
     // Start is called before the first frame update
     void Start()
     {
         breakerBox = GetComponentInParent<ES_BreakerBox>();
+        _powerableComponent = poweredObject.GetComponent<Powerable>();
     }
 
     public void Interact(GameObject Player)
     {
         //switch is on so turn it off
-        if(poweredObject.GetComponent<Powerable>().isOn()){
-            breakerBox.ReturnPower(poweredObject.GetComponent<Powerable>().EnergyToReturn());
-            poweredObject.GetComponent<Powerable>().TurnOff();
+        if(_powerableComponent.isOn()){
+            breakerBox.ReturnPower(_powerableComponent.EnergyToReturn());
+            _powerableComponent.TurnOff();
         }
         else{ //switch is off so turn it on
-            if(breakerBox.UsePower(poweredObject.GetComponent<Powerable>().EnergyToReturn())){ //check if theres enough power, this changes the power indicators
+            if(breakerBox.UsePower(_powerableComponent.EnergyToReturn())){ //check if theres enough power, this changes the power indicators
                 poweredObject.GetComponent<Powerable>().TurnOn();
             }
             else{
--- Assets/Scripts/Electricity/Powerable.cs
+++ Assets/Scripts/Electricity/Powerable.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+//objects part of the electrical system should implement this
+//ES_Lights.cs is an example of how it is implemented
 public interface Powerable
 {
     void TurnOn();  
